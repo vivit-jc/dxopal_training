7399ad663a08d4543ffb43d0897930b6330d1bec
vivit_jc <kazyu.vanilla@gmail.com>
2023-08-29 16:26:11 +0900

    first commit

---
+++ main.rb
@@ -0,0 +1,33 @@
+require 'dxopal'
+include DXOpal
+
+x = 0                         # x座標の変数
+y = 220                         # y座標の変数
+count = 0
+bullets = []
+
+Window.load_resources do
+  Window.bgcolor = C_BLACK
+  img = Image.load("man.png")
+  man = Sprite.new(x,y,img)
+  enemy = Sprite.new(320,30,Image.new(20,20,C_WHITE))
+  man.scale_x = 0.1
+  man.scale_y = 0.1
+  
+  Window.loop do
+    man.x += Input.x*2              # 横方向の入力でx座標を変更
+    man.y += Input.y*2              # 縦方向の入力でy座標を変更
+    count += 1
+    if(count%20 == 0)
+      bullets.push([Sprite.new(enemy.x,enemy.y,Image.new(5,5,C_WHITE),])
+    end
+    bullets.each do |b|
+      b.x
+      b.y += 
+    end
+    
+    man.draw
+
+    
+  end
+end
