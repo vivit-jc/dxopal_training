27ce26a5c4bafeb83ee2644223ba1f320ba4660b
vivit_jc <kazyu.vanilla@gmail.com>
2023-08-29 16:29:23 +0900

    疑似奇数弾追加

--- main.rb
+++ main.rb
@@ -19,15 +19,14 @@ Window.load_resources do
     man.y += Input.y*2              # 縦方向の入力でy座標を変更
     count += 1
     if(count%20 == 0)
-      bullets.push([Sprite.new(enemy.x,enemy.y,Image.new(5,5,C_WHITE),])
+      bullets.push([Sprite.new(enemy.x,enemy.y,Image.new(5,5,C_WHITE),(enemy.y-man.y)/(enemy.x-man.x)])
     end
     bullets.each do |b|
-      b.x
-      b.y += 
+      b[0].x += 3
+      b[0].y += b[1]*3
+      b[0].draw
     end
     
     man.draw
-
-    
   end
 end
